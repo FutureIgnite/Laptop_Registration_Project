718891c6f8bbffbe7413e9ef4a7af5a4360a4f75
Kimtai04-cloud <ckimtai04@gmail.com>
2025-04-06 14:36:36 +0300

    Improved UI

--- DevAuth.c
+++ DevAuth.c
@@ -22,18 +22,18 @@ int main (int argc, char** argv){
         system ("clear");
         printf ("Invalid input, please try again.\n");
     }
-    printf ("\n.............................................\n\n");
-    printf ("student name: %s\n", entry->student.student_name);
-    printf ("stdudent reg number: %s\n", entry->student.reg_no);
-    printf ("student phone number: %s\n", entry->student.phone_no);
-    printf ("student year of study: %s\n", entry->student.y_of_study);
+    printf ("\n\t=============STUDENT RECORD===========================\n\n");
+    printf ("\t\tstudent name: %s\n", entry->student.student_name);
+    printf ("\t\tstdudent reg number: %s\n", entry->student.reg_no);
+    printf ("\t\tstudent phone number: %s\n", entry->student.phone_no);
+    printf ("\t\tstudent year of study: %s\n", entry->student.y_of_study);
     if (strcmp(entry->t_stamp.t_registration, "NULL"))
-        printf ("date of registration: %s\n", entry->t_stamp.t_registration);
+        printf ("\t\tdate of registration: %s\n", entry->t_stamp.t_registration);
     else
-        fprintf (stderr, "Date of registration not set\n");
-    printf ("laptop model: %s\n", entry->laptop.model);
-    printf ("laptop serial number: %s\n", entry->laptop.serial_no);
-    printf ("\n............................................\n\n");
+        fprintf (stderr, "\t\tDate of registration not set\n");
+    printf ("\t\tlaptop model: %s\n", entry->laptop.model);
+    printf ("\t\tlaptop serial number: %s\n", entry->laptop.serial_no);
+    printf ("\n\t=====================================================\n\n");
     free (entry);
     
     return 0; 
