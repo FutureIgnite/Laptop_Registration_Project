b4754b2225050d15a84c0d20b79c6a79f701bef6
Kimtai04-cloud <ckimtai04@gmail.com>
2025-04-10 17:23:19 +0300

    created a method to lookup for existense of a student using reg_no

--- srcs/search_student.c
+++ srcs/search_student.c
@@ -1,21 +1,53 @@
-#include "header_files.h"
+#include "../includes/header_files.h"
 
-Entries* input (char *buff){
-    fgets (buff, sizeof(buff), stdin);
-    if (strchr(buff, '\n'))
-        buff[strcspn (buff, "\n")] = '\0';
-    Entries* entry = check_if_exists (buff);
-    return entry;
-}
 
 //lookup in the database for the object
 
-Entries* check_if_exists (const char *buff){
-    Entries *stdnt;
-    if (!(stdnt = (Entries*) malloc (sizeof(Entries)))){
-        fprintf (stderr, "Error %s", strerror(errno));
+Entry* lookup_db (const char *buff) {
+    Entry *stdnt;
+
+    if (!(stdnt = (Entry*) malloc (sizeof(Entry)))) {
+        fprintf (stderr, "Error allocating memory (student object): %s\n", strerror(errno));
+        return NULL;
     }
+
+    sqlite3 *db;
+
+    if (sqlite3_open ("records.db", &db) != SQLITE_OK) {
+        fprintf (stderr, "Error opening db (search): %s\n", sqlite3_errmsg (db));
+        sqlite3_close (db);
+        return NULL;
+    }
+
+    sqlite3_stmt *stmt;
+
+    const char *sql = "SELECT * FROM students WHERE reg_no = (?)";
+
+    if (sqlite3_prepare_v2 (db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf (stderr, "Error preparing statement (search): %s\n", sqlite3_errmsg (db));
+        sqlite3_close (db);
+        return NULL;
+    }
+
+    sqlite3_bind_text (stmt, 1, buff, -1, SQLITE_TRANSIENT);
+
+    if (sqlite3_step (stmt) == SQLITE_ROW) {
+        strcpy (stdnt -> student.student_name,   (char *)sqlite3_column_text (stmt, 2));
+        strcpy (stdnt -> student.reg_no,         (char *)sqlite3_column_text (stmt, 3));
+        strcpy (stdnt -> student.phone_no,       (char *)sqlite3_column_text (stmt, 4));
+        strcpy (stdnt -> student.y_of_study,     (char *)sqlite3_column_text (stmt, 5));
+
+        strcpy (stdnt -> laptop.model,           (char *)sqlite3_column_text (stmt, 6));
+        strcpy (stdnt -> laptop.serial_no,       (char *)sqlite3_column_text (stmt, 7));
+
+        strcpy (stdnt -> t_stamp.t_registration, (char *)sqlite3_column_text (stmt, 8));
         
+        return stdnt;
+    }
+
+    sqlite3_close (db);
+    return NULL;        
+
 }
 
 
