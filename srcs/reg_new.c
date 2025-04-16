2701de3234737e4f7039f336fd9b11118253c3ff
Kimtai04-cloud <ckimtai04@gmail.com>
2025-04-16 07:55:47 +0300

    Used ncurses functions to get input and print to the WINDOW object

--- srcs/reg_new.c
+++ srcs/reg_new.c
@@ -1,141 +1,181 @@
 #include "../includes/header_files.h"
 
-int register_student (Entry* entry) {
-   init_current_date (entry);
+int register_student (Entry* entry, WINDOW* child) {
+   box (child, 4, 4);
+   init_current_date (entry, child);
    
-   printf ("%s%s\n\t               STUDENT DETAILS                           \n%s%s",
-           TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
+   wprintw (child, "\n\t\t\tSTUDENT DETAILS\n");
+   wrefresh (child);
 
    label1:
 
-   printf ("%s\t\tEnter student name:  %s", TC_BLU, TC_NRM);
-   
-   if (fgets (entry -> student.student_name, sizeof (entry -> student.student_name), stdin) != NULL) {
-      if (isEmpty (entry -> student.student_name[0], "Student Name")) {
-         clear_console ();
+   wprintw (child, "\t\tEnter student name:  ");
+   wrefresh (child);
+
+   if (wgetstr (child, entry -> student.student_name) != ERR) {
+      if (isEmpty (entry -> student.student_name[0], "Student Name", child)) {
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label1;
       }
       if (strlen (entry -> student.student_name) < 4) {
-         fprintf (stderr, "\t\tName is too short.\n");
-         clear_console ();
+         wprintw (child, "\t\tName is too short.\n");
+         wrefresh (child);
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label1;
       }
       add_terminator (entry -> student.student_name, sizeof (entry -> student.reg_no),
-                      strlen (entry -> student.reg_no)); //check for instance of newline and replace it with null
+                      strlen (entry -> student.reg_no), child);
    }else {
-      fprintf (stderr, "\t\tError reading student name %s\n", strerror (errno));
-      return  (REG_ERR); //REG_ERR macro defined in variables.h exands to 1
+      wprintw (child, "\t\tError reading student name %s\n", strerror (errno));
+      wrefresh (child);
+      return  (REG_ERR);
    }
 
    label2:
 
-   printf ("%s\t\tEnter student registration number:  %s", TC_BLU, TC_NRM);
-   
-   if ((fgets (entry -> student.reg_no, sizeof (entry -> student.reg_no), stdin)) != NULL) {
-      if (isEmpty (entry -> student.reg_no[0], "Student Registration Number")) {
+   wprintw (child, "\t\tEnter student registration number:  ");
+   wrefresh (child);
+
+   if ((wgetstr (child, entry -> student.reg_no) != ERR)) {
+      if (isEmpty (entry -> student.reg_no[0], "Student Registration Number", child)) {
          goto label2;
       }
 
       if (strlen (entry -> student.reg_no) < 6) {
-         fprintf (stderr, "\t\tRegistration number too short.\n");
-         clear_console ();
+         wprintw (child, "\t\tRegistration number too short.\n");
+         wrefresh (child);
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label2;
       }
 
       for (char *ch = entry -> student.reg_no; *ch != '\0'; ++ch) {
          if (!isalnum (*ch) && *ch != '/' && *ch != '\0' && *ch != '\n') {
-            fprintf (stderr, "\t\tError: Registration number contains invalid characters.\n");
-            clear_console ();
+            wprintw (child, "\t\tError: Registration number contains invalid characters.\n");
+            wrefresh (child);
+            wclear (child);
+            box (child, 4, 4);
+            wrefresh (child);
             goto label2;
          }
       }
       add_terminator (entry -> student.reg_no, sizeof (entry -> student.reg_no),
-                      strlen (entry -> student.reg_no)); 
+                      strlen (entry -> student.reg_no), child); 
    } else {
-      fprintf (stderr, "\t\tError reading registration number.\n");
+      wprintw (child, "\t\tError reading registration number.\n");
+      wrefresh (child);
       return (REG_ERR);
    }
 
    label3:
 
-   printf ("%s%s\t\tEnter student phone number: ", TC_BLU, TC_NRM);
-   
-   if ((fgets (entry -> student.phone_no, sizeof (entry -> student.phone_no), stdin)) != NULL) {
+   wprintw (child, "\t\tEnter student phone number: ");
+   wrefresh (child);
+
+   if ((wgetstr (child, entry -> student.phone_no) != ERR)) {
       if (strlen (entry -> student.phone_no) < 10) {
-         fprintf (stderr, "\t\tPhone number too short\n");
-         clear_console ();
+         wprintw (child, "\t\tPhone number too short\n");
+         wrefresh (child);
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label3;
       }
       for (char *p = entry -> student.phone_no; *p != '\0'; ++p) {
          if (!isdigit (*p) && *p != '\n') {
-            fprintf (stderr, "\t\tError: Phone number contains invalid characters.\n");
-            clear_console ();
+            wprintw (child, "\t\tError: Phone number contains invalid characters.\n");
+            wrefresh (child);
+            wclear (child);
+            box (child, 4, 4);
+            wrefresh (child);
             goto label3;
          }
       }
       add_terminator (entry -> student.phone_no, sizeof (entry -> student.reg_no),
-                      strlen (entry -> student.reg_no));
+                      strlen (entry -> student.reg_no), child);
    }else {
-      fprintf (stderr, "\t\tError reading phone number.\n");
+      wprintw (child, "\t\tError reading phone number.\n");
+      wrefresh (child);
       return (REG_ERR);
    }
    
    label4:
 
-   printf ("%s%s\t\tEnter student year of study: ", TC_BLU, TC_NRM);
-   
-   if((fgets (entry -> student.y_of_study, sizeof (entry -> student.y_of_study), stdin)) != NULL) {
-      if (isEmpty (entry -> student.y_of_study[0], "Year of study")) {
+   wprintw (child, "\t\tEnter student year of study: ");
+   wrefresh (child);
+
+   if((wgetstr (child, entry -> student.y_of_study)) != ERR) {
+      if (isEmpty (entry -> student.y_of_study[0], "Year of study", child)) {
          goto label4;
       }      
       for (char *ch = entry -> student.y_of_study; *ch != '\0'; ++ch) {
          if (!isalpha (*ch) && *ch != '\n') {
-            fprintf (stderr, "\t\tError: year of study contains invalid characters.\n");
-            clear_console ();
+            wprintw (child, "\t\tError: year of study contains invalid characters.\n");
+            wrefresh (child);
+            wclear (child);
+            box (child, 4, 4);
+            wrefresh (child);
             goto label4;
          }
       }   
       add_terminator (entry -> student.y_of_study, sizeof (entry -> student.reg_no),
-                      strlen (entry -> student.reg_no));
+                      strlen (entry -> student.reg_no), child);
    }else {
-      fprintf (stderr, "\t\tError reading year of study.\n");
+      wprintw (child, "\t\tError reading year of study.\n");
+      wrefresh (child);
       return  (REG_ERR);
    }
    
-   return (REG_OK); //REG_OK macro defined in variables.h expands to 0
+   return (REG_OK);
 }
 
-int register_laptop (Entry* Laptop) {
-   printf ("%s%s\n\t               LAPTOP DETAILS                            \n%s%s", 
-           TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
+int register_laptop (Entry* Laptop, WINDOW* child) {
+   box (child, 4, 4);
+   wprintw (child, "\n\t\t\tLAPTOP DETAILS \n");
+   wrefresh (child);
 
    label5:
 
-   printf ("%s%s\t\tEnter laptop model: ", TC_BLU, TC_NRM);
-   
-   if (fgets (Laptop -> laptop.model, sizeof (Laptop -> laptop.model), stdin) != NULL) {
-      if (isEmpty (Laptop -> laptop.model[0], "Laptop model")) {
+   wprintw (child, "\t\tEnter laptop model: ");
+   wrefresh (child);
+
+   if (wgetstr (child, Laptop -> laptop.model) != ERR) {
+      if (isEmpty (Laptop -> laptop.model[0], "Laptop model", child)) {
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label5;
       }
       add_terminator (Laptop -> laptop.model, sizeof (Laptop -> student.reg_no),
-                      strlen (Laptop -> student.reg_no));
-   }else {
-      fprintf (stderr, "\t\tError reading laptop model %s\n", strerror (errno));
+                      strlen (Laptop -> student.reg_no), child);
+   } else {
+      wprintw (child, "\t\tError reading laptop model %s\n", strerror (errno));
+      wrefresh (child);
       return  (REG_ERR);
    }
 
    label6:
 
-   printf ("%s%s\t\tEnter laptop serial number: ", TC_BLU, TC_NRM);
-   
-   if (fgets (Laptop -> laptop.serial_no, sizeof (Laptop -> laptop.serial_no), stdin) != NULL) {
-      if (isEmpty (Laptop -> laptop.serial_no[0], "Serial Number")) {
+   wprintw (child, "\t\tEnter laptop serial number: ");
+   wrefresh (child);
+
+   if (wgetstr (child, Laptop -> laptop.serial_no) != ERR) {
+      if (isEmpty (Laptop -> laptop.serial_no[0], "Serial Number", child)) {
+         wclear (child);
+         box (child, 4, 4);
+         wrefresh (child);
          goto label6;
       }
       add_terminator (Laptop -> laptop.serial_no, sizeof (Laptop -> student.reg_no),
-                      strlen (Laptop -> student.reg_no));
-   }else {
-      fprintf (stderr, "\t\tError reading laptop serial number %s\n", strerror (errno));
+                      strlen (Laptop -> student.reg_no), child);
+   } else {
+      wprintw (child, "\t\tError reading laptop serial number %s\n", strerror (errno));
+      wrefresh (child);
       return  (REG_ERR);
    }
 
