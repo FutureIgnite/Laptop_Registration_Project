f368a748ed060151419d335b8e608832cd1c64e9
Kimtai04-cloud <ckimtai04@gmail.com>
2025-04-16 07:56:27 +0300

    Used ncurses functions to handle inputs and outputs

--- srcs/ui.c
+++ srcs/ui.c
@@ -1,106 +1,65 @@
 #include "../includes/header_files.h"
+#define R(str) wrefresh (str)
 
-void guide (int *opt) {
-    printf ("\n\n\t%s%s                    CHOOSE OPERATION                       %s%s\n\n", 
-             TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
 
-    printf ("%s%s\t\t0 :  AUTHENTICATE STUDENT\n", TC_YEL, TC_NRM);
-    printf ("%s%s\t\t1 :  REGISTER STUDENT\n", TC_YEL, TC_NRM);
-    printf ("\t\t\tInput: ");
-    scanf ("%i", &(*opt));
-    flush_input_buff();
+char guide (WINDOW * child) {//menu of the available choice of operations
+    
+    box (child, 4, 4);
+
+    mvwprintw (child, 1, 1, "\n\n\t================CHOOSE OPERATION================"); R(child);
+
+    mvwprintw (child, 2, 1, "\t\t[A] :  AUTHENTICATE STUDENT\n"); R(child);
+    mvwprintw (child, 3, 1, "\t\t[R] :  REGISTER STUDENT\n"); R(child);
+    mvwprintw (child, 4, 1, "\t\t[L] :  REPORT LOST LAPTOP\n"); R(child);
+    mvwprintw (child, 5, 1, "\t\t[H] :  HELP\n"); R(child);
+    mvwprintw (child, 6, 1, "\t\t[Q] :  QUIT THE PROGRAM\n"); R(child);
+
+    mvwprintw (child, 7, 1, "\t\tInput: "); R(child);
+    char opt;
+    opt = wgetch (child);
+    return opt;
 }
 
-int prompt (Entry *entry) {
-    int opt = 0, counter = 0; // option
-    char res = 'Y', des = 'N';//response and descision
-    char reg_no[20];
-    Entry *stdnt = NULL;
-
-    do  {
-        guide (&opt);
-        switch (opt) {
-            case 1:
-                do {
-                    system ("clear");
-                    if (register_student (entry) == REG_ERR) {
-                        fprintf (stderr, "\t\tCannot register student\n");
-                        break;
-                    }
-             
-                    if (register_laptop (entry) == REG_ERR) {
-                        fprintf (stderr, "\t\tCannot register laptop\n");
-                        break;
-                    }
-
-                    if (save_data (entry)) 
-                        printf ("\t\tStudent not saved\n");
-                    else
-                        printf ("\t\tStudent saved successfully\n");
-                    
-                    printf ("\t\tDo you want to register another student? (Y/N) : ");
-                    scanf ("%c", &res);
-                    flush_input_buff ();
-                    ++counter;
-                } while (res == 'Y');
-                printf ("\t\t%i students successfully registered\n", counter);
-                sleep (1);
-                system ("clear");
-                break;
-            case 0:
-                do {
-                    printf ("\n\n\t\t%s%s Enter students registation number: ", TC_B_BLU, TC_NRM);
-                    fgets (reg_no, sizeof (reg_no), stdin);
-                    add_terminator (reg_no, sizeof (reg_no), strlen (reg_no));
-                    if (!(stdnt = lookup_db (reg_no))){
-                        fprintf (stderr, "\t\tStudent dont exists\n");
-                        goto label;
-                    } else {
-                        display (stdnt);
-                    }
-                    printf (" \t\tDoes the serial provided same as in the system ? (Y/N): ");
-                    scanf ("%c", &res);
-                    flush_input_buff ();
-                    if (res == 'N'){
-                        printf ("                                 %sREPORTING...%s                                   \n",
-                                TC_RED, TC_NRM);
-                        if (flag_student (reg_no)) {
-                            fprintf (stderr, " \t\tCannot report student\n");
-                        }
-                    }
-                    label:
-                    printf (" \t\tAuthenticate another student (Y/N): ");
-                    scanf ("%c", &res);
-                    flush_input_buff ();
-                    system ("clear");
-                } while (res == 'Y');
-                break;            
+void help_method (WINDOW * child) {
+    mvwprintw (child, 4, 1, "\n\n\t\tEnter [0] to check if the student owns the laptop"); R(child);
+    mvwprintw (child, 5, 1, "\t\t type N/Y to flag student if trying to authenticate someone else laptop\n*****>"); R(child);
+    while (1) {
+        wprintw (child, "Press [B] to go back to the main menu\n");
+        char opt = wgetch (child);
+        if (opt == 'B' || opt == 'b') {
+            break;
         }
-        
-        printf ("\n\n%s%s\t\tQUIT (Q) CONTINUE (C) %s%s", TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
-        printf ("Input: ");
-        scanf ("%c", &des);
-        flush_input_buff ();
-    } while (des == 'C' || des == 'c');
-
-    return 0;
+    }
+    return;
 }
 
-void display (Entry *entry){
-    printf ("\n\n\t%s%s            STUDENT RECORD                               %s%s\n\n", 
-              TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
-    printf ("\t\tstudent name            : %s\n", entry->student.student_name);
-    printf ("\t\tstdudent reg number     : %s\n", entry->student.reg_no);
-    printf ("\t\tstudent phone number    : %s\n", entry->student.phone_no);
-    printf ("\t\tstudent year of study   : %s\n", entry->student.y_of_study);
+void display (Entry *entry, WINDOW * child) {
+    box (child, 4, 4);
+
+    mvwprintw (child, 1, 1, "\n\n\t==============STUDENT RECORD=================\n\n"); R(child);
+    mvwprintw (child, 2, 1, "\t\tName               : %s\n", entry->student.student_name); R(child);
+    mvwprintw (child, 3, 1, "\t\tReg Number         : %s\n", entry->student.reg_no); R(child); 
+    mvwprintw (child, 4, 1, "\t\tPhone Number       : %s\n", entry->student.phone_no); R(child);
+    mvwprintw (child, 5, 1, "\t\tYear of Study      `: %s\n", entry->student.y_of_study); R(child);
     
-    if (strcmp (entry->t_stamp.t_registration, "NULL"))
-        printf ("\t\tdate of registration    : %s\n", entry->t_stamp.t_registration);
-    else
-        fprintf (stderr, "\t\tDate of registration not set\n");
-    
-    printf ("\t\tlaptop model            : %s\n", entry->laptop.model);
-    printf ("\t\tlaptop serial number    : %s\n", entry->laptop.serial_no);
-    printf ("\n\t%s%s                                                         %s%s\n\n",
-              TC_CYN, TC_BG_WHT, TC_NRM, TC_BG_NRM);
+    if (strcmp (entry->t_stamp.t_registration, "NULL")) {
+        mvwprintw (child, 6, 1, "\t\tdate of registration    : %s\n", entry->t_stamp.t_registration); R(child);
+    } else {
+        mvwprintw (child, 6, 1, "\t\tDate of registration not set\n"); R(child);
+    }
+    mvwprintw (child, 7, 1, "\t\tLaptop Model            : %s\n", entry->laptop.model); R(child);
+    mvwprintw (child, 8, 1, "\t\tLaptop Serial Number    : %s\n", entry->laptop.serial_no); R(child);
+    mvwprintw (child, 9, 1, "\n\t============================================\n\n"); R(child);
+
+    /*mvwprintw (child, 10, 1, "\t\tPress [B] to go back to the main menu\n"); R(child);
+    char choice;
+    while (1) {
+        choice = wgetch(child);
+        if (choice == 'B' || choice == 'b') {
+            delwin(child);
+            break;
+        }
+    }*/
+
+    free(entry);
 }
