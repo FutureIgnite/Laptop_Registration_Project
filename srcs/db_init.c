4094a6fc53ced3ec86a5d8b1f3e8bc836ccce15c
Kimtai04-cloud <ckimtai04@gmail.com>
2025-04-16 07:52:04 +0300

    ...

--- srcs/db_init.c
+++ srcs/db_init.c
@@ -1,15 +1,15 @@
 #include "../includes/header_files.h"
-
+#define R refresh ()
 int db_init() {
 
     sqlite3 *db;
 
     if (sqlite3_open ("records.db", &db) != SQLITE_OK) {
-        fprintf(stderr, "Can't initialize database: %s\n", sqlite3_errmsg(db));
+        printw ("Can't initialize database: %s\n", sqlite3_errmsg(db)); R;
         return 1;
     }
 
-    fprintf(stdout, "Opened database successfully\n");
+    printw ("Opened database successfully\n"); R;
     
     // SQL to create tables
     
@@ -34,13 +34,13 @@ int db_init() {
     const char *sqlTail;
 
     if (sqlite3_prepare_v2 (db, sql_st, -1, &stmt, &sqlTail) != SQLITE_OK){
-        fprintf (stderr, "Error praparing statement (student_table): %s\n", sqlite3_errmsg (db));
+        printw ("Error praparing statement (student_table): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_close (db);
         return 1;
     }
 
     if (sqlite3_step (stmt) != SQLITE_DONE){
-        fprintf (stderr, "Error executing statement (student_table): %s\n", sqlite3_errmsg (db));
+        printw ("Error executing statement (student_table): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_finalize (stmt);
         sqlite3_close (db);
         return 1;
@@ -49,13 +49,13 @@ int db_init() {
     sqlite3_finalize (stmt);
 
     if (sqlite3_prepare_v2 (db, sqlTail, -1, &stmt, NULL) != SQLITE_OK){
-        fprintf (stderr, "Error preparing statement (laptops_table): %s\n", sqlite3_errmsg (db));
+        printw ("Error preparing statement (laptops_table): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_close (db);
         return 1;
     }
 
     if (sqlite3_step (stmt) != SQLITE_DONE){
-        fprintf (stderr, "Error executing statement (laptops_table): %s\n", sqlite3_errmsg (db));
+        printw ("Error executing statement (laptops_table): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_finalize (stmt);
         sqlite3_close (db);
         return 1;
@@ -64,13 +64,13 @@ int db_init() {
     sqlite3_finalize (stmt);
 
     if (sqlite3_prepare_v2 (db, sql_foreign_key_enable, -1, &stmt, NULL) != SQLITE_OK){
-        fprintf (stderr, "Error preparing statement (foreign_key): %s\n", sqlite3_errmsg (db));
+        printw ("Error preparing statement (foreign_key): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_close (db);
         return 1;
     }
 
     if (sqlite3_step (stmt) != SQLITE_DONE){
-        fprintf (stderr, "Error executing statement (foreign_keys): %s\n", sqlite3_errmsg (db));
+        printw ("Error executing statement (foreign_keys): %s\n", sqlite3_errmsg (db)); R;
         sqlite3_finalize (stmt);
         sqlite3_close (db);
         return 1;
@@ -81,4 +81,186 @@ int db_init() {
 
     return 0;
 }
+
+Entry* lookup_db (const char *buff) {
+    Entry *stdnt;
+
+    if (!(stdnt = (Entry*) malloc (sizeof(Entry)))) {
+        printw ("Error allocating memory (student object): %s\n", strerror(errno)); R;
+        return NULL;
+    }
+
+    sqlite3 *db;
+
+    if (sqlite3_open ("records.db", &db) != SQLITE_OK) {
+        printw ("Error opening db (search): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        return NULL;
+    }
+
+    sqlite3_stmt *stmt;
+
+    const char *sql = "SELECT * FROM students WHERE reg_no = (?)";
+
+    if (sqlite3_prepare_v2 (db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        printw ("Error preparing statement (search): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        return NULL;
+    }
+
+    sqlite3_bind_text (stmt, 1, buff, -1, SQLITE_TRANSIENT);
+
+    int id;
+    char *col_text;
+
+    if (sqlite3_step (stmt) == SQLITE_ROW) {
+        
+        col_text =  (char *)sqlite3_column_text (stmt, 1);
+        strncpy (stdnt -> student.student_name,   col_text ? col_text : "", sizeof (stdnt -> student.student_name) -1);
+
+        col_text =  (char *)sqlite3_column_text (stmt, 2);
+        strncpy (stdnt -> student.reg_no,         col_text ? col_text : "", sizeof (stdnt -> student.reg_no) -1);
+
+        col_text =  (char *)sqlite3_column_text (stmt, 3);
+        strncpy (stdnt -> student.phone_no,       col_text ? col_text : "", sizeof (stdnt -> student.phone_no) -1);
+
+        col_text =  (char *)sqlite3_column_text (stmt, 4);
+        strncpy (stdnt -> student.y_of_study,     col_text ? col_text : "", sizeof (stdnt -> student.y_of_study) -1);
+        
+        col_text =  (char *)sqlite3_column_text (stmt, 5);
+        strncpy (stdnt -> t_stamp.t_registration,  col_text ? col_text : "", sizeof (stdnt -> t_stamp.t_registration) -1);
+        
+        id = sqlite3_column_int (stmt, 0);
+    }
+
+    sqlite3_finalize (stmt);
+
+    const char *sql_l = "SELECT * FROM laptops WHERE student_id = (?)";
+
+    if (sqlite3_prepare_v2 (db, sql_l, -1, &stmt, NULL) != SQLITE_OK) {
+        printw ("Error preparing statement (f_laptop): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        return NULL;
+    }
+
+    sqlite3_bind_int (stmt, 1, id);
+
+    if (sqlite3_step (stmt) == SQLITE_ROW){
+        
+        col_text = (char *)sqlite3_column_text (stmt, 2);
+        strncpy (stdnt -> laptop.model,           col_text ? col_text : "", sizeof (stdnt -> laptop.model) -1);
+
+        col_text =  (char *)sqlite3_column_text (stmt, 3);
+        strncpy (stdnt -> laptop.serial_no,       col_text ? col_text : "", sizeof (stdnt -> laptop.serial_no) -1);
+
+       
+        sqlite3_finalize (stmt);
+        sqlite3_close (db);
+        return stdnt;
+    }
+
+    sqlite3_close (db);
+    free (stdnt);
+    return NULL;        
+
+}   
+
+int flag_student (const char *reg_no) {
+    sqlite3 *db;
     
+    if (sqlite3_open ("records.db", &db)) {
+        printw ("Cannot open database (flaging): %s\n", sqlite3_errmsg (db)); R;
+        return 1;
+    }
+
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE students SET flaged = 'Y' WHERE reg_no = (?)";
+    
+    if (sqlite3_prepare_v2 (db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        printw ("Error preparing statement (flaging): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        return 1;
+    }
+
+    sqlite3_bind_text (stmt, 1, reg_no, -1, SQLITE_TRANSIENT);
+
+    if (sqlite3_step (stmt) != SQLITE_DONE) {
+        printw ("Failed to flag student: %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_finalize (stmt);
+        sqlite3_close (db);
+        return 1;
+    }
+
+    sqlite3_finalize (stmt);
+    sqlite3_close (db);
+
+    return 0;
+}
+
+int save_data (Entry *entry){
+    sqlite3 *db;
+    
+    if (sqlite3_open ("records.db", &db) != SQLITE_OK){
+        printw ("Error opening database (write_db): %s\n", sqlite3_errmsg (db)); R;
+        free (entry);
+        return  1;
+    }
+
+    sqlite3_stmt *stmt;
+    
+    const char *sql_s = "INSERT INTO students ("
+        "name, reg_no, phone_no, year_of_study, date_of_reg) VALUES (?, ?, ?, ?, ?)";
+
+    const char *sql_l = "INSERT INTO laptops (student_id, model, serial_no) VALUES (?, ?, ?)";
+
+    if (sqlite3_prepare_v2 (db, sql_s, -1, &stmt, NULL) != SQLITE_OK) {
+        printw ("Error preparing statement (write_db_s): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        free (entry);
+        return 1;
+    }
+
+    sqlite3_bind_text (stmt, 1, entry -> student.student_name, -1,   SQLITE_TRANSIENT);
+    sqlite3_bind_text (stmt, 2, entry -> student.reg_no, -1,         SQLITE_TRANSIENT);
+    sqlite3_bind_text (stmt, 3, entry -> student.phone_no, -1,       SQLITE_TRANSIENT);
+    sqlite3_bind_text (stmt, 4, entry -> student.y_of_study, -1,     SQLITE_TRANSIENT);
+    sqlite3_bind_text (stmt, 5, entry -> t_stamp.t_registration, -1, SQLITE_TRANSIENT);
+
+    if (sqlite3_step (stmt) != SQLITE_DONE) {
+        printw ("Error executing statement (write_db_s): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_finalize (stmt);
+        sqlite3_close    (db);
+        free (entry);
+        return 1;
+    }
+    
+    int student_id = sqlite3_last_insert_rowid (db);
+
+    sqlite3_finalize (stmt);
+
+    if (sqlite3_prepare_v2 (db, sql_l, -1, &stmt, NULL)) {
+        printw ("Error preparing statement (write_db_l): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_close (db);
+        free (entry);
+        return 1;
+    }
+    
+    sqlite3_bind_int  (stmt,  1, student_id);
+    sqlite3_bind_text (stmt, 2, entry -> laptop.model, -1,     SQLITE_TRANSIENT);
+    sqlite3_bind_text (stmt, 3, entry -> laptop.serial_no, -1, SQLITE_TRANSIENT);
+
+    if (sqlite3_step (stmt) != SQLITE_DONE){
+        printw ("Error executing statement (write_db_l): %s\n", sqlite3_errmsg (db)); R;
+        sqlite3_finalize (stmt);
+        sqlite3_close    (db);
+        free (entry);
+        return 1;
+    }
+
+    sqlite3_finalize (stmt);
+    
+    sqlite3_close    (db);
+
+    free (entry);
+    return 0;
+}
